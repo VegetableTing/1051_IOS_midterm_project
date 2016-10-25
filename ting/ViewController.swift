1e5101640a9c9dc4a5d4d049c1d69be8a771ed27
user 09 <user_09@mac-09deimac.local>
2016-10-25 11:29:54 +0800

    Initial Commit

---
+++ ting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ting
+//
+//  Created by user_05 on 2016/10/11.
+//  Copyright © 2016年 user_05. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
